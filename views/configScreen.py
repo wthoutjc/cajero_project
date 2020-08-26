576e1df2282408315cb60ae50540411c29be2551
Alejandro Puentes Camargo <brianpc200@gmail.com>
2020-08-25 20:06:26 -0500

    commit

--- views/configScreen.py
+++ views/configScreen.py
@@ -3,12 +3,12 @@ from views.Builder import *
 class configScreen():
 
     def setScreen(self, posicion):
-        self.ii2 = Image.open(self.getCarrusel(posicion))
+        self.ii2 = Image.open(self.getBuilders(posicion))
         self.ii2.thumbnail((465,365))
         self.ii2 = ImageTk.PhotoImage(self.ii2)
         return self.ii2
 
-    def getCarrusel(self, posicion):
+    def getBuilders(self, posicion):
         self.app = BuilderManager()
         self.opciones = [BuilderPantalla0(), BuilderPantalla1(),BuilderPantalla2(), BuilderAgrario(), BuilderBancolombia(), BuilderDavivienda()]
         self.app.setBuilder(self.opciones[posicion])
